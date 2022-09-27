a2abbaf75c93c76135b90843968dea3226fa7e10
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 19:43:14 +0000

    Created function to iterate through a list of questions

--- survey.js
+++ survey.js
@@ -5,30 +5,57 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const profile = {
-  name: {
-    question: "What's your name? Nicknames are also acceptable :)"
-  },
-  activity: {
-    question: "What's an activity you like doing?"
-  },
-  music: {
-    question: "What do you listen to while doing that?"
-  },
-  meal: {
-    question: "Which meal is your favourite (eg: dinner, brunch, etc.)"
-  },
-  food: {
-    question: "What's your favourite thing to eat for that meal?"
-  },
-  sport: {
-    question: "Which sport is your absolute favourite?"
-  },
-  superpower: {
-    question: "What is your superpower? In a few words, tell us what you are amazing at!"
-  }
+const questions = ['How are you?', 'What are you doing?'];
+
+const askQuestions = function(questions) {
+  let questionIndex = 0;
+
+  const askQuestion = function() {
+    rl.question(questions[questionIndex], (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      questionIndex++;
+      if (questionIndex === questions.length) {
+        rl.close();
+        return;
+      }
+      askQuestion();
+    });
+  };
+
+  askQuestion();
 };
 
-for (const key in profile) {
-  console.log(profile[key].question);
-}
+askQuestions(questions);
+
+// const profile = {
+//   name: {
+//     question: "What's your name? Nicknames are also acceptable :)"
+//   },
+//   activity: {
+//     question: "What's an activity you like doing?"
+//   },
+//   music: {
+//     question: "What do you listen to while doing that?"
+//   },
+//   meal: {
+//     question: "Which meal is your favourite (eg: dinner, brunch, etc.)"
+//   },
+//   food: {
+//     question: "What's your favourite thing to eat for that meal?"
+//   },
+//   sport: {
+//     question: "Which sport is your absolute favourite?"
+//   },
+//   superpower: {
+//     question: "What is your superpower? In a few words, tell us what you are amazing at!"
+//   }
+// };
+
+// for (const key in profile) {
+//   rl.question(profile[key].question, (answer) => {
+//     console.log(`Thank you for your valuable feedback: ${answer}`);
+//     rl.pause();
+//   })
+// }
+
+// rl.close();
