1b9d2d1c17ff5f1a7f91f8fd4e8384ef08701f06
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 19:46:53 +0000

    Added array with assignment questions

--- survey.js
+++ survey.js
@@ -5,14 +5,23 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const questions = ['How are you?', 'What are you doing?'];
+const questions = [
+  "What's your name? Nicknames are also acceptable :)",
+  "What's an activity you like doing?",
+  "What do you listen to while doing that?",
+  "Which meal is your favourite (eg: dinner, brunch, etc.)",
+  "What's your favourite thing to eat for that meal?",
+  "Which sport is your absolute favourite?",
+  "What is your superpower? In a few words, tell us what you are amazing at!"
+];
+let answers = [];
 
 const askQuestions = function(questions) {
   let questionIndex = 0;
 
   const askQuestion = function() {
-    rl.question(questions[questionIndex], (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+    rl.question(questions[questionIndex] + ' ', (answer) => {
+      answers.push(answer);
       questionIndex++;
       if (questionIndex === questions.length) {
         rl.close();
@@ -26,36 +35,3 @@ const askQuestions = function(questions) {
 };
 
 askQuestions(questions);
-
-// const profile = {
-//   name: {
-//     question: "What's your name? Nicknames are also acceptable :)"
-//   },
-//   activity: {
-//     question: "What's an activity you like doing?"
-//   },
-//   music: {
-//     question: "What do you listen to while doing that?"
-//   },
-//   meal: {
-//     question: "Which meal is your favourite (eg: dinner, brunch, etc.)"
-//   },
-//   food: {
-//     question: "What's your favourite thing to eat for that meal?"
-//   },
-//   sport: {
-//     question: "Which sport is your absolute favourite?"
-//   },
-//   superpower: {
-//     question: "What is your superpower? In a few words, tell us what you are amazing at!"
-//   }
-// };
-
-// for (const key in profile) {
-//   rl.question(profile[key].question, (answer) => {
-//     console.log(`Thank you for your valuable feedback: ${answer}`);
-//     rl.pause();
-//   })
-// }
-
-// rl.close();
